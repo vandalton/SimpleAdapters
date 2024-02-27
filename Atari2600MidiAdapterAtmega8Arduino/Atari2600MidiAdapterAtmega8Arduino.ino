e3bc01a87c364dc9f7777f7a55d001482bc0cfe5
Slawomir Krysztowiak <slawomir.krysztowiak@ecom.software>
2024-02-28 00:19:38 +0100

    Atari 2600 Midi adapter WIP

--- Atari2600MidiAdapterAtmega8Arduino/Atari2600MidiAdapterAtmega8Arduino.ino
+++ Atari2600MidiAdapterAtmega8Arduino/Atari2600MidiAdapterAtmega8Arduino.ino
@@ -1,3 +1,5 @@
+#include <MIDI.h>
+
 // Atari 2600 joystick pinout - seen from the front
 // \1 2 3 4 5/
 //  \6 7 8 9/
@@ -5,14 +7,14 @@
 // 1 - row 1, 2 - row 2, 3 - row 3, 4 - row 4, 5 - col 1
 // 6 - col 3, 7 - 5V (HIGH), 8 - GND (LOW), 9 - col 2
 
-#define OUT_COL_3 15 // connect digital pin 15 of Atmega to pin 6 of joystick port
-#define OUT_COL_2 14
-#define OUT_COL_1 13
+#define OUT_COL_3 9 // connect digital pin 7 of Atmega to pin 6 of joystick port
+#define OUT_COL_2 8
+#define OUT_COL_1 7
 
-#define IN_ROW_4 12
-#define IN_ROW_3 11
-#define IN_ROW_2 6
-#define IN_ROW_1 5
+#define IN_ROW_4 6
+#define IN_ROW_3 5
+#define IN_ROW_2 4
+#define IN_ROW_1 3
 
 int inputs[4] = { IN_ROW_1, IN_ROW_2, IN_ROW_3, IN_ROW_4 };
 int outputs[3] = { OUT_COL_1, OUT_COL_2, OUT_COL_3 };
@@ -45,12 +47,15 @@ struct PortInit {
 
 PortInit portInit;
 
-void setup() {
-  // put your setup code here, to run once:
+MIDI_CREATE_DEFAULT_INSTANCE();
 
+void setup() {
+  MIDI.begin(MIDI_CHANNEL_OMNI); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (MIDI.read())
+  {
+    MIDI.sendNoteOn(42, 127, 1);
+  }
 }
